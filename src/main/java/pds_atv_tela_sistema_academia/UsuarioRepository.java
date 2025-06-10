48b75a4d61b42a2c8c5ec6666ff6be513a67b4f1
Art5019 <arthur.p31@aluno.ifsc.edu.br>
2025-06-10 11:14:54 -0300

    Backup no sistema de repository, iniciando refatoração do código

---
+++ src/main/java/pds_atv_tela_sistema_academia/UsuarioRepository.java
@@ -0,0 +1,66 @@
+package pds_atv_tela_sistema_academia;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+public class UsuarioRepository {
+	
+	UsuarioService usuario;
+	
+	public void criarUsuario(String nome, int exp, String endereco, char genero, String senha) {
+		int matricula = MenuLogin.cadastrados;
+		if(nome.length() > 4 && endereco.length() > 4 && senha.length() > 4) {
+			File novoUsuario = new File("data/users/"+matricula+".txt");
+			FileWriter escritor;
+			
+			try {
+				novoUsuario.createNewFile();
+				escritor = new FileWriter(novoUsuario);
+				escritor.write("NOM:"+nome+";\nEND:"+endereco+";\nGEN:"+genero+";\nEXP:"+exp+";\nSEN:"+senha+";\n");
+				
+				escritor.close();
+				JOptionPane.showMessageDialog(null, "Usuário criado com matrícula "+matricula, "Sucesso", 3);
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", 2);
+			}
+		}else {
+			JOptionPane.showMessageDialog(null, "Os campos estão demasiadamente pequenos!", "Erro!", 2);
+			
+		}
+	}
+		
+		public void usuario(String nome, int exp, String endereco, char genero, String senha, char mode) {
+			int matricula = MenuLogin.cadastrados;
+			if(mode == 'U') {
+				matricula = Integer.parseInt(MenuLogin.getLogged());
+			}
+			
+			if(nome.length() > 4 && endereco.length() > 4 && senha.length() > 4) {
+				File arquivoUsuario = new File("data/users/"+matricula+".txt");
+				FileWriter escritor;
+				
+				try {
+					arquivoUsuario.createNewFile();
+					escritor = new FileWriter(arquivoUsuario);
+					escritor.write("NOM:"+nome+";\nEND:"+endereco+";\nGEN:"+genero+";\nEXP:"+exp+";\nSEN:"+senha+";\n");
+					
+					escritor.close();
+					if(mode == 'U') {
+						JOptionPane.showMessageDialog(null, "Usuário atualizado", "Sucesso", 3);
+					}else {
+						JOptionPane.showMessageDialog(null, "Usuário criado com matrícula "+matricula, "Sucesso", 3);
+					}
+					
+				} catch (IOException e) {
+					JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", 2);
+				}
+			}else {
+				JOptionPane.showMessageDialog(null, "Os campos estão demasiadamente pequenos!", "Erro!", 2);
+				
+			}
+		}
+
+}
--- src/main/java/pds_atv_tela_sistema_academia/UsuarioService.java
+++ src/main/java/pds_atv_tela_sistema_academia/UsuarioService.java
@@ -13,57 +13,8 @@ public class UsuarioService {
 	private PopupsService popups = new PopupsService();
 	
 	public void criarUsuario(String nome, int exp, String endereco, char genero, String senha) {
-		int matricula = MenuLogin.cadastrados;
-		if(nome.length() > 4 && endereco.length() > 4 && senha.length() > 4) {
-			File novoUsuario = new File("data/users/"+matricula+".txt");
-			FileWriter escritor;
-			
-			try {
-				novoUsuario.createNewFile();
-				escritor = new FileWriter(novoUsuario);
-				escritor.write("NOM:"+nome+";\nEND:"+endereco+";\nGEN:"+genero+";\nEXP:"+exp+";\nSEN:"+senha+";\n");
-				
-				escritor.close();
-				JOptionPane.showMessageDialog(null, "Usuário criado com matrícula "+matricula, "Sucesso", 3);
-			} catch (IOException e) {
-				JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", 2);
-			}
-		}else {
-			JOptionPane.showMessageDialog(null, "Os campos estão demasiadamente pequenos!", "Erro!", 2);
-			
-		}
+		U
 	}
-		
-		public void usuario(String nome, int exp, String endereco, char genero, String senha, char mode) {
-			int matricula = MenuLogin.cadastrados;
-			if(mode == 'U') {
-				matricula = Integer.parseInt(MenuLogin.getLogged());
-			}
-			
-			if(nome.length() > 4 && endereco.length() > 4 && senha.length() > 4) {
-				File arquivoUsuario = new File("data/users/"+matricula+".txt");
-				FileWriter escritor;
-				
-				try {
-					arquivoUsuario.createNewFile();
-					escritor = new FileWriter(arquivoUsuario);
-					escritor.write("NOM:"+nome+";\nEND:"+endereco+";\nGEN:"+genero+";\nEXP:"+exp+";\nSEN:"+senha+";\n");
-					
-					escritor.close();
-					if(mode == 'U') {
-						JOptionPane.showMessageDialog(null, "Usuário atualizado", "Sucesso", 3);
-					}else {
-						JOptionPane.showMessageDialog(null, "Usuário criado com matrícula "+matricula, "Sucesso", 3);
-					}
-					
-				} catch (IOException e) {
-					JOptionPane.showMessageDialog(null, e.getMessage(), "Erro!", 2);
-				}
-			}else {
-				JOptionPane.showMessageDialog(null, "Os campos estão demasiadamente pequenos!", "Erro!", 2);
-				
-			}
-		}
 		
 		public boolean validar(String validar, String senha) {
 			if(senha.equals(Leitor.getSenha(validar))) {
