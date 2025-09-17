87096be715b64ddb6b40ddb13ab34da1bdacdcd7
Arthur-Pereira <60709850+Arthur-Pereira-5019@users.noreply.github.com>
2025-09-17 18:57:12 -0300

    Finalized most of the Exercise Repository class

---
+++ src/main/java/pds_atv_tela_sistema_academia/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package pds_atv_tela_sistema_academia.exceptions;
+
+public class ResourceNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String message) {
+		super(message);
+	}
+
+}
--- src/main/java/pds_atv_tela_sistema_academia/treinos/ExercicioSQLRepository.java
+++ src/main/java/pds_atv_tela_sistema_academia/treinos/ExercicioSQLRepository.java
@@ -2,8 +2,10 @@ package pds_atv_tela_sistema_academia.treinos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import pds_atv_tela_sistema_academia.exceptions.ResourceNotFoundException;
 import pds_atv_tela_sistema_academia.services.BancoDeDados;
 
 public class ExercicioSQLRepository {
@@ -28,12 +30,38 @@ public class ExercicioSQLRepository {
 		return e;
 	}
 	
-	public Exercicio deletarExercicio(String nomeTreino) {
-		
+	public void deletarExercicio(String nomeTreino) {
+		String sql = "delete from gym.exercicios where nomeTreino = ?";
+		try {
+			PreparedStatement s = c.prepareStatement(sql);
+			s.setString(0, nomeTreino);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public Exercicio encontrarExercicio(String nomeExercicio) {
-		
+	public Exercicio encontrarExercicio(String nomeExercicio) throws ResourceNotFoundException {
+		String sql = "select from gym.exercicios where nomeTreino = ?";
+		ResultSet resultSet = null;
+		try {
+			resultSet = c.prepareStatement(sql).executeQuery();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		Exercicio ex = null;
+		try {
+			resultSet.next();
+			ex = new Exercicio();
+			ex.setGrupo(resultSet.getString("grupo"));
+			ex.setGrupo(resultSet.getString("nomeTreino"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(ex == null || ex.nomeTreino.isEmpty() || ex.nomeTreino == null) {
+			throw new ResourceNotFoundException("Exercise not found");
+		}
+		return ex;
 	}
 
 }
